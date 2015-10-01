9a1e4ca00a9b7bc3c50f4b662c618bef7ca3295a
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-10-01 23:48:04 +0800

    Create class for KissManga directory route constants

---
+++ app/src/main/java/kishido/smoochmanga/site/KMRoute.java
@@ -0,0 +1,24 @@
+package kishido.smoochmanga.site;
+
+/**
+ * Created by syspaulo on 10/1/2015.
+ */
+public class KMRoute {
+
+    public static final String BASE_URL = "http://www.kissmanga.com/";
+
+    public static final String MANGA_LIST = "MangaList/";
+    public static final String SEARCH_MANGA = "Search/Manga/";
+    public static final String SEARCH_AUTHOR = "Search/AuthorArtist/";
+
+    // sort
+    public static final String BY_POPULAR = "MostPopular/";
+    public static final String BY_LATEST = "LatestUpdate/";
+    public static final String BY_NEWEST = "Newest/";
+
+    // filter
+    public static final String BY_GENRE = "Genre/";
+    public static final String BY_STATUS = "Status/";
+
+    public static final String MANGA_INFO = "Manga/";
+}
