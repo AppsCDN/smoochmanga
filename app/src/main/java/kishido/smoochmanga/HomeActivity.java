a7d8e829020ba7df36b91fbba3b4b6a7cadfbf33
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-10-04 23:05:35 +0800

    Clean imports

--- app/src/main/java/kishido/smoochmanga/HomeActivity.java
+++ app/src/main/java/kishido/smoochmanga/HomeActivity.java
@@ -1,12 +1,7 @@
 package kishido.smoochmanga;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
